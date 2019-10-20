958fb53586e1a98a170bf6012a4dbb93c2e150d9
ishtiyaque <ishtiyaque.2197@yahoo.com>
2019-10-20 10:24:41 -0700

    added run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+docker build -t us.gcr.io/cs291-f19/project3_${CS291_ACCOUNT} .
+docker run -it --rm \
+  -p 3000:3000 \
+  -v ~/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json \
+  us.gcr.io/cs291-f19/project3_${CS291_ACCOUNT}
+
