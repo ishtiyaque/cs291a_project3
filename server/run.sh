af728fa8759dee184bf36a78c0461ec0e970a806
ishtiyaque <ishtiyaque.2197@yahoo.com>
2019-10-22 12:59:55 -0700

    switch to thin

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,4 @@
-docker build -t us.gcr.io/cs291-f19/project3_${CS291_ACCOUNT} .
+docker build --no-cache -t us.gcr.io/cs291-f19/project3_${CS291_ACCOUNT} .
 docker run -it --rm \
   -p 4000:3000 \
   -v ~/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json \
