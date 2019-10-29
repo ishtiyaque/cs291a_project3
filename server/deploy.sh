2f52a6fa295fd9fa99fe587707013e4b0a11a3c0
Abtin Bateni <abtinbateni@gmail.com>
2019-10-28 22:02:12 -0700

    update run.sh

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,2 @@
 docker build -t mrbathat/project3_${CS291_ACCOUNT} .
-docker run -it --rm \
-  -p 4000:3000 \
-  -v ~/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json \
-  mrbathat/project3_${CS291_ACCOUNT}
+docker push mrbathat/project3_abtinbateni
